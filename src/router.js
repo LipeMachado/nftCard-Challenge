0961f0c10912454ec797c27eadbc44250427d237
LipeMachado <silvalipe765@gmail.com>
2022-03-24 01:39:10 -0300

    Fixed Imports

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { InitialPage } from './Components/NftCard';
+import { NftCard } from './Components/NftCard/';
 
 export default function MainRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<InitialPage />}/>
+            <Route path="/" element={<NftCard />}/>
         </Routes>
     );
 }
